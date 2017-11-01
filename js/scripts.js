e57b2df137ef827d7b81b2fc1f8e611e618b9538
Colin Worf and John Sawiris <colin@>
2017-11-01 16:51:52 -0700

    did everything haha

---
+++ js/scripts.js
@@ -0,0 +1,86 @@
+//Backend Logic
+function Player(name, active, totalScore, tempScore) {
+  this.name = name;
+  this.active = active;
+  this.totalScore = totalScore;
+  this.tempScore = tempScore;
+}
+
+Player.prototype.pass = function(){
+  // if (this.tempScore === 0) {
+     this.active = !this.active;
+    return this.totalScore += this.tempScore;
+  // }
+  // else {
+  //   this.active = !this.active;
+  //   return this.totalScore += this.tempScore;
+  // }
+}
+Player.prototype.zero = function(){
+  return this.tempScore = 0;
+}
+
+Player.prototype.turn = function(rollScore) {
+  if (rollScore === 1) {
+   this.tempScore = 0;
+    return this.pass();
+  } else {
+    this.tempScore += rollScore;
+  }
+
+}
+
+//Frontend Logic
+$(document).ready(function() {
+
+var player1 = new Player("player1", true, 0, 0);
+var player2 = new Player("player2", false, 0, 0);
+
+
+  $("#roll").click(function() {
+    var roll = Math.floor(Math.random() * 6) + 1;
+
+    if (player1.active === true){
+      player1.turn(roll);
+      $('.turn-box.p1').text(roll);
+      $('#p1-score').text(player1.totalScore)
+      if (roll === 1) {
+        player2.active = true;
+        $('.turn-box').toggleClass('active');
+        $('.turn-box').html("<span style='font-size:.5em'>Your Turn!</span>");
+        $('.current-player').toggleClass('other');
+        $('#p1-score').text(player1.totalScore)
+      }
+
+    } else {
+      player2.turn(roll);
+      $('.turn-box.p2').text(roll);
+      $('#p2-score').text(player2.totalScore)
+      if (roll === 1) {
+        player1.active = true;
+        $('.turn-box').toggleClass('active');
+        $('.turn-box').html("<span style='font-size:.5em'>Your Turn!</span>");
+        $('.current-player').toggleClass('other');
+        $('#p2-score').text(player2.totalScore)
+      }
+    }
+
+    console.log(roll);
+    console.log(player1);
+    console.log(player2);
+  });//Roll click ends
+  $("#pass").click(function() {
+    player1.pass();
+    player1.zero();
+    player2.pass();
+    player2.zero();
+    $('.turn-box').toggleClass('active');
+    $('.turn-box').html("<span style='font-size:.5em'>Your Turn!</span>");
+    $('.current-player').toggleClass('other');
+    $('#p2-score').text(player2.totalScore);
+    $('#p1-score').text(player1.totalScore)
+    console.log("pass")
+    console.log(player1);
+    console.log(player2);
+  });//Pass click ends
+});//Ready ends
