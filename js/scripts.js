2133c39398a5a5b57fd4e212c7fb9a4a5bada1d7
Colin Worf <colin.worf@gmail.com>
2017-11-01 23:46:03 -0700

    add turn score box and animations

--- js/scripts.js
+++ js/scripts.js
@@ -43,11 +43,15 @@ var player2 = new Player("player2", false, 0, 0);
     if (player1.active === true){
       player1.turn(roll);
       $('.turn-box.p1').text(roll);
-      $('#p1-score').text(player1.totalScore)
+      $('.turn-score.p1').text(player1.tempScore);
       if (roll === 1) {
+        $('.oops.p1').addClass('active').delay(2000).queue(function(next){
+            $(this).removeClass('active');
+            next();
+        });
         player2.active = true;
-        $('.turn-box').toggleClass('active');
-        $('.turn-box').html("<span style='font-size:.5em'>Your Turn!</span>");
+        $('.turn-box, .turn-score').toggleClass('active');
+        $('.turn-box').html("<span style='font-size:.5em'>Your<br>Turn!</span>");
         $('.current-player').toggleClass('other');
         $('#p1-score').text(player1.totalScore)
       }
@@ -55,11 +59,15 @@ var player2 = new Player("player2", false, 0, 0);
     } else {
       player2.turn(roll);
       $('.turn-box.p2').text(roll);
-      $('#p2-score').text(player2.totalScore)
+      $('.turn-score.p2').text(player2.tempScore);
       if (roll === 1) {
+        $('.oops.p2').addClass('active').delay(2000).queue(function(next){
+            $(this).removeClass('active');
+            next();
+        });
         player1.active = true;
-        $('.turn-box').toggleClass('active');
-        $('.turn-box').html("<span style='font-size:.5em'>Your Turn!</span>");
+        $('.turn-box, .turn-score').toggleClass('active');
+        $('.turn-box').html("<span style='font-size:.5em'>Your<br>Turn!</span>");
         $('.current-player').toggleClass('other');
         $('#p2-score').text(player2.totalScore)
       }
@@ -74,8 +82,8 @@ var player2 = new Player("player2", false, 0, 0);
     player1.zero();
     player2.pass();
     player2.zero();
-    $('.turn-box').toggleClass('active');
-    $('.turn-box').html("<span style='font-size:.5em'>Your Turn!</span>");
+    $('.turn-box, .turn-score').toggleClass('active');
+    $('.turn-box').html("<span style='font-size:.5em'>Your<br>Turn!</span>");
     $('.current-player').toggleClass('other');
     $('#p2-score').text(player2.totalScore);
     $('#p1-score').text(player1.totalScore)
